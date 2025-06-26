69ec8d0bfd914c50ae52294d4b4124fc19a56c5f
leocodeio <basicleo787@gmail.com>
2025-06-26 13:30:21 +0530

    feat(web docker done): web docker done

--- apps/web/start.sh
+++ apps/web/start.sh
@@ -5,4 +5,5 @@
 # run, which is why this file exists in the first place.
 # Learn more: https://community.fly.io/t/sqlite-not-getting-setup-properly/4386
 
+cd /usr/src/app/packages/db && pnpm db:migrate
 cd /usr/src/app/apps/web && pnpm start
