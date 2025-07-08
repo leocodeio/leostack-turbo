841711eec8c6af63bacfa1c1524781a125bbeb04
leocodeio <basicleo787@gmail.com>
2025-07-08 19:55:05 +0530

    feat(Added docker files): Added docker files

--- apps/web/start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh -ex
-
-# This file is how Fly starts the server (configured in fly.toml). Before starting
-# the server though, we need to run any prisma migrations that haven't yet been
-# run, which is why this file exists in the first place.
-# Learn more: https://community.fly.io/t/sqlite-not-getting-setup-properly/4386
-
-cd /usr/src/app/packages/db && pnpm db:migrate
-cd /usr/src/app/apps/web && pnpm start
